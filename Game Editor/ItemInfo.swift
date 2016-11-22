0e55a28accc4535edb928e08953c4bcd2beea567
fluidity <dudeguy@dudes-imac.local>
2016-11-22 00:37:26 -0500

    jk

---
+++ Game Editor/ItemInfo.swift
@@ -0,0 +1,23 @@
+//
+//  ItemInfo.swift
+//  Game Editor
+//
+//  Created by Dude Guy on 11/22/16.
+//  Copyright © 2016 Dude Guy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItemInfo: UIViewController {
+
+
+
+
+
+
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
