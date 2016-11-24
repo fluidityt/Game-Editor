9c34e32652b5b31e2d09c5d9b43fcaa818cfee55
fluidity <dudeguy@Dudes-iMac.local>
2016-11-24 08:53:13 -0500

    jaj

--- console/EquipableItems.swift
+++ console/EquipableItems.swift
@@ -1,115 +1,140 @@
-//
-//  EquipableItems.swift
-//  Game Editor
-//
-//  Created by Dude Guy on 11/24/16.
-//  Copyright © 2016 Dude Guy. All rights reserved.
-//
+	//
+	//  EquipableItems.swift
+	//  Game Editor
+	//
+	//  Created by Dude Guy on 11/24/16.
+	//  Copyright © 2016 Dude Guy. All rights reserved.
+	//
 
-import Foundation
+	import Foundation
 
-// ********** EQUIPABLE ITEMS.swift ************* \\
 
-// MARK: - TOP -
+	// MARK: - TOP -
 
-fileprivate let ud = UserDefaults.standard
+	// ********************************************** \\
+	// ********** EQUIPABLE ITEMS.swift ************* \\
+	// ********************************************** \\
+
 
 // MARK: -
 
-/// Enums can be used to generate things via the Editor, which store stuff in an array/dict:
-enum Keys:String {
-/**/case item = "Item->"
-/**//**/enum Item: String {
-/**//**//**/case equip = "Item->Equip->"
-/**//**//**//**/enum Equip: String {
-/**//**//**//**//**/case head = "Item->Equip->Head->"
-/**//**//**//**//**/case chest = "Item->Equip->Chest->"
-/**//**//**//**/}
-/**//**/}
-}
+	/// Used everywhere:
+	enum Slot: String {
+		case
+		head =  "head",
+		chest = "chest",
+		hands = "hands",
+		feet = "feet",
+		legs = "legs",
+		finger = "finger"
 
-/// Enums used in the Equippable Item Page:
-enum Slot: String {
-	case
-	head =  "head",
-	chest = "chest"
+		static func load(_ ue: String) -> Slot { return Slot(rawValue: ue)! }
+	}
 
-	/*func key() -> String {
-		return (Keys.Item.equip.rawValue + self.rawValue)
-	}*/
+	/// Enums can be used to generate things via the Editor, which store stuff in an array/dict:
+	fileprivate enum Keys:String {
+	/**/case item = "Item->"
+	/**//**/enum Item: String {
+	/**//**//**/case equip = "Item->Equip->"
+	/**//**//**//**/enum Equip: String {
+	/**//**//**//**//**/case head = "Item->Equip->Head->"
+	/**//**//**//**//**/case chest = "Item->Equip->Chest->"
+	/**//**//**//**/}
+	/**//**/}
+	}
 
-	static func load(_ ue: String) -> Slot { return Slot(rawValue: ue)! }
-}
+	fileprivate let ud = UserDefaults.standard
+
+	/* Lists: */
+
+	fileprivate var listOfItems:[String: String] = [:]
+	fileprivate let listOfItemsKey = "Equip Items Key"
+
+	fileprivate func list(_ name: String,_ slot: Slot) { listOfItems[name] = slot.rawValue }
+	fileprivate func saveList() { ud.set(listOfItems, forKey: listOfItemsKey); ud.synchronize() }
+	fileprivate func loadList() { listOfItems = ud.value(forKey: listOfItemsKey) as! [String:String] }
+
+	func listTest() {
+		list("Dagger",.hands)
+		list("Sword",.hands)
+		list("BronzeHelm",.head)
+
+		saveList()
+		print(NSDictionary(dictionary: listOfItems))
+
+	}
 
 // MARK: -
 
-/// The actual item instance:
-struct EquipableHeadItem {
+	/// The actual item instance:
+	struct Equipable {
 
+	/* Properties: */ let
 
-/* Properties: */ let
+		name: String,
+		slot: Slot,
+		prot: Int
 
-	name: String,
-	slot: Slot,
-	prot: Int
+	/* Inits: */
 
-/* Inits: */
-
-	init() {
-		name = "Sword"; slot = .chest; prot = 45
-	}
-
-}
-
-/* Protocol: */
-
-extension EquipableHeadItem: CanSave {
-
-	private typealias EHI = EquipableHeadItem // For convenience
-
-	static func key(_ itemName: String) -> String {
-		return(Keys.Item.Equip.head.rawValue + itemName)
-	}
-
-	func saveToUD() {
-		let info: [String:String]
-			=	["name": name,
-			 	 "slot": slot.rawValue,
-			 	 "prot": String(prot)]
-
-		ud.set(info, forKey: EHI.key(name))
-		ud.synchronize()
-	}
-
-	init?(loadFromName named: String) {
-
-		if let dict = ud.value( forKey: EHI.key(named) ) as! [String: String]? {
-
-			func val(_ ue: String) -> String { return dict[ue]! }
-			func intVal(_ ue: String) -> Int { return Int(dict[ue]!)! }
-
-			name = val("name")
-			slot = Slot.load(val("slot"))
-			prot = intVal("prot")
+		init(name: String, slot: Slot, prot: Int) {						// Full init
+			self.name = name; self.slot = slot; self.prot = prot
 		}
 
-		else { return nil }
 	}
 
-}//EoC
+// MARK: - Protocol:
 
+	extension Equipable {
 
-// Model to hold it:
-enum EquipHeadPageInfo {
+		private typealias E = Equipable // For convenience
 
-	static var name = ""
+		func key() -> String { return(slot.rawValue + name)	}
 
-	static func clickSave(toItem: EquipableHeadItem){
-		// for stuff in self, copy to item
+		static func key(name2: String, slot2: Slot) -> String { return(slot2.rawValue + name2) }
 
+		func saveToUD() {
+			let info: [String:String]
+				=	["name": name,
+					 "slot": slot.rawValue,
+					 "prot": String(prot)]
+
+			ud.set(info, forKey: key())
+			ud.synchronize()
+		}
+
+		init?(loadFromName named: String, forSlot slotted: Slot) {
+
+			let key1 = E.key(name2: named, slot2: slotted)
+			if let dict = ud.value( forKey: key1 ) as! [String: String]? {
+
+				func val(_ ue: String) -> String { return dict[ue]! }
+				func intVal(_ ue: String) -> Int { return Int(dict[ue]!)! }
+
+				name = val("name")
+				slot = Slot.load(val("slot"))
+				prot = intVal("prot")
+			}
+
+			else { return nil }
+		}
+
+	}//EoC
+
+// MARK: -
+
+	// Model to hold it:
+	enum EquipHeadPageInfo {
+
+		static var name = ""
+
+		static func clickSave(toItem: Equipable){
+			// for stuff in self, copy to item
+
+		}
 	}
 
-}
+// MARK: - Bottom -
 
 
 
--- console/main.swift
+++ console/main.swift
@@ -5,4 +5,19 @@
 //  Created by Dude Guy on 11/23/16.
 //  Copyright © 2016 Dude Guy. All rights reserved.
 //
+fileprivate func tests() {
 
+
+	let sword = Equipable(name: "Sword", slot: .chest, prot: 45)
+
+	print(sword)
+
+	let loadedSword = Equipable.init(loadFromName: "Sword", forSlot: .chest)
+
+}; tests()
+
+listTest()
+
+
+
+/*  ************ ***************** ************** ************* ************ ******************/ print("")
