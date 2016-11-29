afe40292dbeb002818ef0333a801708b4976f175
dudeguy <3403>
2016-11-28 21:27:58 -0500

    auto-commit hackybook November 28, 2016 9:27 PM

--- Game Editor/ItemView.swift
+++ Game Editor/ItemView.swift
@@ -33,19 +33,17 @@ extension UIViewController {
 
 class ItemView: UITableViewController {
 
-/* Fields: */ private var
-              // Can I use a dictionary for this?
-              // Can use udef.load for this? Followed by = Equipable(loadFrom: )?
-							equipment		= [(name: "Crash Inc", key: "Crash Key")],
-							insertTop		= false
-
-/* MARK: Funcs: */
-
-	func grabCell(indexPath: IndexPath) -> UITableViewCell {
-		return tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-	}
-
-  private func loadEquipment() {
+  // MARK: - clickNew:
+  
+  @IBAction func clickNew(_ sender: Any) {
+  }
+  
+ 
+// MARK: - viewDidLoad:
+  
+		private var equipment = [(name: "Crash Inc", key: "Crash Key")]
+  
+    private func loadEquipment() {
     
     equipment = []
     for (key, val) in udef.loadEquipmentKeysAsDictVals() {
@@ -58,28 +56,46 @@ class ItemView: UITableViewController {
       equipment.append((ourOnlyItem.name, ourOnlyItem.key()))
     }
   }
-  
-
-/* MARK: - Overrides: */
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
     loadEquipment()
 	}
 
-	/// didSelectRow()
-	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+  // MARK: - didSelectRow():
+  
+  private var isDeleteMode = false
+  
+  @IBAction func clickDeleteMode(_ sender: Any) {
+  }
+  private func doDeleteMode(selectedRow: Int) {
+  }
+  private func doRegularMode(selectedRow: Int) {
+  
     globalEquipItemStuff.item = Equipable.loadFromKey( ziggly(equipment[indexPath.row].key) )
     presentVC(named: "Item Info")
+  }
+  
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    isDeleteMode
+    ? doDeleteMode(selectedRow: indexPath.row)
+    : doRegularMode(selectedRow: indexPath.row)
+    
 	}
 
-	/// makeRows()
+    // MARK: - numberOfRowsInSection:
+    
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
 		-> Int {
       return equipment.count;
 	}
 
-	/// grabCellAtRow()
+// MARK: - cellForRowAt():
+    
+func grabCell(indexPath: IndexPath) -> UITableViewCell {
+		return tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+	}
+    
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
 		-> UITableViewCell {
 
